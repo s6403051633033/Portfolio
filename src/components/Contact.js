c2bec4b0b223783b81a2b35f6776df905164726b
s6403051633033 <s6403051633033@email.kmutnb.ac.th>
2024-11-07 03:00:47 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,7 +13,7 @@ function Contact() {
                         <h2 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="400">Contact Me</h2>
                         <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="800">For more information contact me</p>
                         <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="800">Email : Poodit555@gmail.com </p>
-                        <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="800">Number : +0986642055 </p>
+                        <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="800">Number : +66 986642055 </p>
 
                     </div>
                 </div>
